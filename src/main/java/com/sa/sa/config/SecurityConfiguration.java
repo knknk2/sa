1ed39571431029732243a9ba3d122ee2c97d7730
mtdx <ms2asdk@gmail.com>
2019-08-07 20:56:07 +0300

    lint

--- src/main/java/com/sa/sa/config/SecurityConfiguration.java
+++ src/main/java/com/sa/sa/config/SecurityConfiguration.java
@@ -3,8 +3,6 @@ package com.sa.sa.config;
 import com.sa.sa.security.*;
 import com.sa.sa.security.jwt.*;
 
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
